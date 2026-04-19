2b350a0c97cb056f043dd85868003b66602a9642
riqueamais <henrique.quintino99@gmail.com>
2026-04-19 20:57:40 -0300

    feat: implement delete answer use case and in-memory repository methods, add tests and factories for answer creation

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,7 +1,7 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export interface QuestionsRepository {
-  findById(questionId: string): Promise<Question | null>
+  findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
   create(question: Question): Promise<void>
   delete(question: Question): Promise<void>
---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,31 @@
+import type { AnswersRepository } from '../repositories/answers-repository'
+
+interface DeleteAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+interface DeleteAnswerUseCaseResponse {}
+
+export class DeleteAnswerUseCase {
+  constructor(private AnswersRepository: AnswersRepository) {}
+
+  async execute({
+    authorId,
+    answerId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.AnswersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed to delete this Answer')
+    }
+
+    await this.AnswersRepository.delete(answer)
+
+    return {}
+  }
+}
