3b49d92a8405ae7b03f81e3ed7bb9ea4490c07b0
riqueamais <henrique.quintino99@gmail.com>
2026-04-19 20:17:59 -0300

    feat: implement delete question use case and in-memory repository methods, add faker for test data generation

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,18 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import {
   Question,
   type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: 'Example Question',
-    content: 'This is an example question content.',
-    slug: Slug.create('example-question'),
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraph(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
